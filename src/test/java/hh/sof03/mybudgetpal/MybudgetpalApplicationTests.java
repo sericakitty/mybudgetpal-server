4cc203f2b2fa3b0bef02ef981ffab00f766876f5
sericakitty <63978546+sericakitty@users.noreply.github.com>
2024-11-06 04:28:52 +0200

    Add empty Test file

---
+++ src/test/java/hh/sof03/mybudgetpal/MybudgetpalApplicationTests.java
@@ -0,0 +1,13 @@
+package hh.sof03.mybudgetpal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MybudgetpalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
